b384613d5abfda9b55d6327bb007fc1a1400d5f6
Joanna Fass <fassj14@highpoint.edu>
2018-01-15 19:46:51 -0500

    Quiz App

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -2,7 +2,9 @@
 //  ViewController.swift
 //  Quiz
 //
-//  Created by Joanna Fass on 1/10/18.
+//  Joanna Fass
+//  1/17/18
+//  ~/csc2310/Quiz
 //  Copyright © 2018 Joanna Fass. All rights reserved.
 //
 
@@ -10,16 +12,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
